d772cc75615670cce43a18e15d80553af263956f
Max W. G. Damasceno <maxwilljf@gmail.com>
2020-07-06 08:23:40 -0300

    Imagem da lâmpada e atualizando o consumo

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ function dadosProGrafico (medicoes) {
         labels: datas,
         series: [
             dadosConsumidos,
-            [0, (17/1) * diasUteis, (26/2) * diasUteis]
+            [0, (17/1) * diasUteis, (26/2) * diasUteis, (40/6) * diasUteis]
         ]
     }
 }
@@ -81,8 +81,9 @@ function dadosProGrafico (medicoes) {
 // "Dark mode"
 const csslinkelm = document.getElementById('maincsslink');
 const darkMode = document.getElementById('darkMode');
+
 darkMode.addEventListener('click', (item) => {
-    console.log('Darkmode', item.target);
+    console.log('Darkmode:', item.target);
     console.log('Darkmode.target.dataset', item.target.dataset);
 
     changeDarkMode(item.target, csslinkelm, item.target.dataset.dm)
@@ -92,11 +93,13 @@ function changeDarkMode (darkMode, csslinkelm, mode) {
     console.log(darkMode.dataset);
     if (mode === 'off') {
         darkMode.dataset.dm = 'on';
+        darkMode.src = './images/on.png';
         csslinkelm.href = 'style-dark.css';
     }
     
     if (mode === 'on') {
         darkMode.dataset.dm = 'off';
+        darkMode.src = './images/off.png';
         csslinkelm.href = 'style.css';
     }
 }
